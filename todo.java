4ea0080b70c69edc08d672ee7385a7a201f3d433
Roberto Stagg <robertostagg@Robertos-MacBook-Air.local>
2023-05-04 08:38:34 -0700

    Here's another commit

--- todo.java
+++ todo.java
@@ -14,24 +14,48 @@ public class todo{
 
           // Allows the user to add a task
           if (prompt.toLowerCase().equals("a")){
-              // Todo
+              System.out.println("Enter the name of the task below");
+              String task = input.next();
+              boolean duplicate = false;
+              for (int i = 0; i < todo.size(); i++){
+                  if (todo.get(i).equals(task)){
+                      duplicate = true;
+                      break;
+                  }
+              }
+              // Checking for duplicates
+              if (duplicate){
+                  System.out.println(task + " already exists in your list.");
+                  Thread.sleep(2000);
+                  System.out.print("\033[H\033[2J");
+                  System.out.flush();
+
+
+                }
+                // Add task if no duplicates were found
+                else{
+                    todo.add(task);
+                    System.out.println("The tasked called " + task + " has been added");
+                }
             }
+            
             // User can remove tasks
             else if (prompt.toLowerCase().equals("r")){
-             // Todo
-
+                // Todo
             }
+
             // Allow the user to view the tasks
             else if (prompt.toLowerCase().equals("v")){
-               // Too
+               // Todo
             }
+
             // User can edit a task
             else if (prompt.toLowerCase().equals("e")){
-            // Todo
+                // Todo
             }
+
             // Allows to quit the program
             else if (prompt.toLowerCase().equals("q")){
-        
             System.out.println("You are quitting the program.");
             System.exit(0);
         }
