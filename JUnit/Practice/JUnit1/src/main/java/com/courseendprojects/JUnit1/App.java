8bd6060d8ff6cf9e97d645938f40eeca7f431e4d
RobertoStagg <stagg.roberto@gmail.com>
2023-10-11 10:43:45 -0700

    Initial Commit of first Junit Test Exercises

---
+++ JUnit/Practice/JUnit1/src/main/java/com/courseendprojects/JUnit1/App.java
@@ -0,0 +1,13 @@
+package com.courseendprojects.JUnit1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
